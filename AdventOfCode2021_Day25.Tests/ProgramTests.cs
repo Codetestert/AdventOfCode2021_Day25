e02dd85c89e5408bff68b1cef45766551a190f46
Daniël knippenborg <danielknippenborg@gmail.com>
2025-10-14 17:32:08 +0100

    Add project files.

---
+++ AdventOfCode2021_Day25.Tests/ProgramTests.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+namespace AdventOfCode2021_Day25.Tests
+{
+    public class ProgramTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Arrange
+            string[] example = File.ReadAllLines("TestInput.txt");
+
+            //Act
+            int result = Program.CalculateSteps(example);
+
+            //Assert
+            Assert.Equal(58, result);
+        }
+    }
+}
---
+++ AdventOfCode2021_Day25/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Net.Http.Headers;
+
+namespace AdventOfCode2021_Day25
+{
+    public class Program
+    {
+        static void Main()
+        {
+            //read file
+            //loop through each line
+
+            var lines = File.ReadAllLines("Input.txt");
+            var steps = CalculateSteps(lines);
+            Console.WriteLine($"Answer: {steps}");
+        }
+
+        public static int CalculateSteps(string[] lines)
+        {
+            //DONT simultaneously consider if there's space
+            //east before south
+            //wrap around
+
+            
+            int count = 0;
+            for (int i = 0; i < lines.Length; i++)
+            {
+                count++;
+            }
+
+            return count;
+        }
+    }
+}
